4d1819e750dca00c7da9b541da9e20f89d499938
Arnau Granados <arnau.gra@gmail.com>
2023-03-01 15:33:08 +0100

    final dialog open system

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,8 +1,6 @@
 <script setup>
-
 import Button from "@components/Button.vue";
 import Icon from "@components/Icon.vue";
-import { onMounted } from "vue";
 
 const props = defineProps({
   id: {
@@ -30,49 +28,52 @@ const props = defineProps({
 
 // const emits = defineEmits(["openModal", "closeModal"]);
 
-  /**
-   * Open the modal
-   */
-  function openModal() {
-    document.getElementById(props.id).showModal();
-  }
+/**
+ * Open the modal
+ * @advertise CAN'T FIND A WAY TO DO THE SHOW MODAL WITH VUE 3
+ */
+function openModal() {
+  document.getElementById(props.id).showModal();
+}
 
-  /**
-   * Close the modal
-   */
-  function closeModal() {
-    document.getElementById(props.id).close();
-  }
-
+/**
+ * Close the modal
+ * @advertise CAN'T FIND A WAY TO DO THE SHOW MODAL WITH VUE 3
+ */
+function closeModal() {
+  document.getElementById(props.id).close();
+}
 </script>
 
 <template>
-    <Button
-      @click.prevent="openModal"
-      :textColor="props.textColor"
-      :bgColor="props.bgColor"
-      :hoverBgColor="props.hoverBgColor"
-      :css="props.openCss"
-    >
-      <span v-if="props.buttonText != null">{{ props.buttonText }}</span>
-      <Icon v-if="props.buttonIcon != null" :icon="props.buttonIcon" />
-    </Button>
+  <Button
+    @click.prevent="openModal"
+    :textColor="props.textColor"
+    :bgColor="props.bgColor"
+    :hoverBgColor="props.hoverBgColor"
+    :css="props.openCss"
+  >
+    <span v-if="props.buttonText != null">{{ props.buttonText }}</span>
+    <Icon v-if="props.buttonIcon != null" :icon="props.buttonIcon" />
+  </Button>
 
-  <dialog :id="props.id" type="alert" class=" bg-slate-200 
-  max-w-screen-md w-11/12 rounded-lg z-50 scrollbar-none mt-40
-  backdrop:bg-slate-800 backdrop:opacity-50 backdrop:blur-3xl
-  ">
+  <dialog
+    :id="props.id"
+    type="alert"
+    class="bg-slate-200 max-w-screen-md w-11/12 rounded-lg z-50 scrollbar-none mt-40 backdrop:bg-slate-800 backdrop:opacity-50 backdrop:blur-3xl"
+  >
     <div class="flex justify-end w-full">
-      <Button 
-      @click.prevent="closeModal"
-      textColor="text-zinc-800"
-      bgColor="transparent"
-      hoverBgColor="transparent"
-      css="w-min py-0 p-4 sm:px-0">
+      <Button
+        @click.prevent="closeModal"
+        textColor="text-zinc-800"
+        bgColor="transparent"
+        hoverBgColor="transparent"
+        css="w-min py-0 p-4 sm:px-0"
+      >
         <Icon icon="ri-close-line" css="text-3xl" />
       </Button>
     </div>
-    <div class="mt-5 ">      
+    <div class="mt-5">
       <slot />
     </div>
   </dialog>
